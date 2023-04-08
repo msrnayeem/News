0f7c291e9adf83623290ebe900395be3dcff0d2b
Msr Nayeem <msrnayeem@gmail.com>
2023-04-08 15:28:21 +0600

    services

---
+++ BLL/Services/CategoryService.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Models;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class CategoryService
+    {
+        public static List<Category> GetAll()
+        {
+            return CategoryRepo.GetAll();
+        }
+        public static Category Get(int id)
+        {
+            return CategoryRepo.Get(id);
+        }
+        public static void Add(Category category)
+        {
+            CategoryRepo.Add(category);
+        }
+        public static void Update(Category category)
+        {
+            CategoryRepo.Update(category);
+        }
+        public static void Delete(int id)
+        {
+            CategoryRepo.Delete(id);
+        }
+    }
+}
---
+++ BLL/Services/NewsService.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Models;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class NewsService
+    {
+        public static List<News> GetAll()
+        {
+            return NewsRepo.GetAll();
+        }
+        public static News Get(int id)
+        {
+            return NewsRepo.Get(id);
+        }
+        public static void Add(News news)
+        {
+            NewsRepo.Add(news);
+        }
+        public static void Update(News news)
+        {
+            NewsRepo.Update(news);
+        }
+        public static void Delete(int id)
+        {
+            NewsRepo.Delete(id);
+        }
+    }
+}
