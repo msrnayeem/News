7cbc990741b735244cb0c8c2a5a0c8b0c4dd630d
Msr Nayeem <msrnayeem@gmail.com>
2023-04-08 15:31:19 +0600

    DTOs

---
+++ BLL/DTOs/NewsDTO.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class NewsDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+        public DateTime Date { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
